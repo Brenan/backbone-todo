47fd3b7e8de0492be52c7ebf740e697f4b9c10fd
Brenan Klain <brenanklain@gmail.com>
2013-10-02 15:37:36 -0600

    Adds model and view

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 $(document).ready(function() {
+
+var ToDo = Backbone.Model.extend({
+	defaults:{
+		title: '',
+		completed: false
+	}
+});
+
+var ToDoView = Backbone.View.extend({
+	el: '.todos-list',
+	tagName: "li",
+	className: "todo",
+	template: _.template($("#todo-template").html()),
+	render: function(){
+		this.$el.append(this.template(this.model.toJSON()));
+	}
+});
+
+$("#new-todo").keyup(function(e){
+	if (e.which == 13){
+		var addTask = new ToDoView({
+			model: new ToDo({
+				title: $(this).val(),
+				completed: false
+			})
+		})
+	}
+	addTask.render();
+});
+
+
+
 });
